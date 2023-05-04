6f8d927b5a3e9f86d459f95c0849a2c20d24f897
Giodzz <giordanabbucci@gmail.com>
2023-05-04 15:09:01 -0300

    Aula 49 - Parâmtros tipo referência

--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Estudante.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Estudante.java
@@ -1,2 +1,7 @@
-package academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio;public class Estudante {
+package academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio;
+
+public class Estudante {
+    public String nome;
+    public int idade;
+    public char sexo;
 }
--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/EstudanteTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/EstudanteTest01.java
@@ -1,2 +1,26 @@
-package academy.devdojo.maratonajava.javacore.Bintroducaometodos.test;public class EstudanteTest01 {
+package academy.devdojo.maratonajava.javacore.Bintroducaometodos.test;
+
+import academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio.Estudante;
+import academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio.ImpressoraEstudante;
+
+public class EstudanteTest01 {
+    public static void main(String[] args) {
+        Estudante estudante1 = new Estudante();
+        Estudante estudante2 = new Estudante();
+        ImpressoraEstudante impressora = new ImpressoraEstudante();
+
+        estudante1.nome = "Joãozin";
+        estudante1.idade = 15;
+        estudante1.sexo = 'M';
+
+        estudante2.nome = "Joana";
+        estudante2.idade = 17;
+        estudante2.sexo = 'F';
+
+        // quando um objeto é passado como parâmetro
+        // é do tipo referência
+        impressora.imprime(estudante1);
+        impressora.imprime(estudante2);
+
+    }
 }
